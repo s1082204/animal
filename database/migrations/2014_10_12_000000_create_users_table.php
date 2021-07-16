8b90279299749ea5c944a25404c53468d2b35ee5
黃重皓 <sokve855@gmail.com>
2021-07-16 21:25:24 +0800

    fix

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -18,7 +18,6 @@ class CreateUsersTable extends Migration
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
-            $table->boolean('is_admin')->defaule(0);
             $table->string('password');
             $table->rememberToken();
             $table->timestamps();
